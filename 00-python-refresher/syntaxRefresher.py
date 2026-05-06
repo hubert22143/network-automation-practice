78ce1a02a21c8d638453910310981819e0b5022e
Hubert Baciak <hubert.baciak@proton.me>
2026-05-06 18:33:45 +0200

    Syntax refresher

--- 00-python-refresher/syntaxRefresher.py
+++ 00-python-refresher/syntaxRefresher.py
@@ -1,20 +1,25 @@
 class Animal:
-    def __init__(self, ableToWalk: bool, ableToSwim: bool , ableToSpeak: bool, name : str , age : int) -> None:
-        self.ableToWalk = ableToWalk
-        self.ableToSwim = ableToSwim
-        self.ableToSpeak = ableToSpeak
+    def __init__(self, able_to_walk: bool, able_to_swim: bool, able_to_speak: bool, name: str, age: int) -> None:
+        self.able_to_walk = able_to_walk
+        self.able_to_swim = able_to_swim
+        self.able_to_speak = able_to_speak
         self.name = name
         self.age = age
 
-    def setAbleToWalk(self,value: bool):
-        self.ableToWalk = value
+    def set_able_to_walk(self, value: bool) -> None:
+        self.able_to_walk = value
+
 
 class Dog(Animal):
-    def __init__(self, breed: str, can_walk: bool , name : str , age : int) -> None:
+    def __init__(self, breed: str, can_walk: bool, name: str, age: int) -> None:
         self.breed = breed
-        super().__init__(can_walk,True,False,name,age)
+        super().__init__(can_walk, True, False, name, age)
+
+    def bark(self) -> None:
+        print("Woof")
 
 
-my_dog = Dog("Labrador" , True , "Noris" , 19)
+my_dog = Dog("Labrador", True, "Noris", 19)
 
 print(my_dog.name)
+my_dog.bark()
